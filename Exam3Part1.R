31f5a14d23d049e2a0404670ce93cfe1f35cd1a6
hardikamuni <muni.hardika@gmail.com>
2014-10-27 08:18:24 -0500

    IT497Exam

---
+++ Exam3Part1.R
@@ -0,0 +1,10 @@
+library(RCurl)
+data <- getURL("https://raw.githubusercontent.com/jrwolf/IT497/master/fitstats.csv")
+mydata <- read.csv(textConnection(data))
+head(mydata)
+
+
+plot(x = mydata$Steps, y = mydata$Miles,
+     xlab = "Steps Walked",
+     ylab = "Miles Walked",
+     cex.lab = 1.5)
