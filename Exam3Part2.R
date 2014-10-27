70004a982e83253f47eafdec7a16e8e1b33d9f2c
hardikamuni <muni.hardika@gmail.com>
2014-10-27 08:14:31 -0500

    Update Exam3Part2.R

--- Exam3Part2.R
+++ Exam3Part2.R
@@ -4,7 +4,7 @@ data <- getURL("https://raw.githubusercontent.com/jrwolf/IT497/master/fitstats.c
 mydata <- read.csv(textConnection(data))
 head(mydata)
 
-plot(x = mydata$Miles, y = mydata$Steps,
-     xlab = "Miles Walked",
-     ylab = "Steps Walked",
+plot(x = mydata$Steps, y = mydata$Miles,
+     xlab = "Steps Walked",
+     ylab = "Miles Walked",
      cex.lab = 1.5)
